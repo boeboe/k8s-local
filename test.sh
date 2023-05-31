4f3f356de17601c2ab7f91fa97346f86aca4453e
Bart Van Bos <bart@tetrate.io>
2023-05-31 19:37:57 +0200

    wip

--- test.sh
+++ test.sh
@@ -18,7 +18,8 @@ start_cluster "test2" "1.25.8" "test2" "192.168.200.0/24";
 # sleep 30 ;
 # remove_cluster "test" "test";
 echo "DONE" ;
-sleep 120 ;
+read -p "Press enter to continue"
+
 remove_cluster "test1" "test1";
 remove_cluster "test2" "test2";
 
